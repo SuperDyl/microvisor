90fe996ba9bf1f2ccfd111f5ff6ee581fc550526
Dylan <SuperDyl19@gmail.com>
2026-04-15 21:40:05 -0600

    Add typescript support.

--- update.ts
+++ update.ts
@@ -1,17 +1,33 @@
-const express = require("express");
-const expressQueue = require("express-queue");
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+import express, { Response } from "express";
+import expressQueue from "express-queue";
+import { exec } from "child_process";
+import fs from "fs";
+import path from "path";
 
-const yargs = require("yargs");
+import yargs from "yargs";
 
-const options = yargs
+type Command = {
+  command: string;
+  directory: string;
+  failOnText?: string;
+};
+
+type ConfigFile = {
+  commands: Command[];
+};
+
+type Options = {
+  port: number;
+  serverName: string;
+  configPath: string;
+};
+
+const options: Options = yargs
   .option("port", {
     alias: "p",
     describe: "The port the server listens on",
     type: "number",
-    check: (port) => {
+    check: (port: number) => {
       if (port < 1 || port > 65535) {
         throw new Error("Port must be between 1 and 65535");
       }
@@ -30,7 +46,7 @@ const options = yargs
     type: "string",
     default: "update-config.json",
   })
-  .demandOption(["port", "serverName"]).argv;
+  .demandOption(["port", "serverName"]).argv as Options;
 
 const port = options.port;
 const serverName = options.serverName;
@@ -40,17 +56,23 @@ const app = express();
 app.use(express.json());
 app.use(expressQueue({ activeLimit: 1, queuedLimit: 2 }));
 
-let config = null;
+let config: ConfigFile | null = null;
 try {
   const configData = fs.readFileSync(configFilePath, "utf8");
   config = JSON.parse(configData);
+  // TODO: Confirm that config is the correct format
 } catch (error) {
   console.error(`Error loading config file '${configFilePath}'`, error);
   process.exit(1);
 }
 
+if (config === null) {
+  console.error(`File was in the wrong format '${configFilePath}'`);
+  process.exit(2);
+}
+
 // Helper function to execute a command in a directory
-const executeCommand = async (command, directory) => {
+async function executeCommand (command: string, directory: string): Promise<{stdout: string, stderr: string}> {
   return new Promise((resolve, reject) => {
     const childProcess = exec(
       command,
@@ -70,7 +92,7 @@ const executeCommand = async (command, directory) => {
       }
     );
 
-    childProcess.on("exit", (code) => {
+    childProcess.on("exit", (code: number) => {
       if (code !== 0) {
         console.error(`Command "${command}" exited with code ${code}`);
         reject(new Error(`Command "${command}" exited with code ${code}`));
@@ -80,7 +102,7 @@ const executeCommand = async (command, directory) => {
 };
 
 // Function to process the queue
-const processCommands = async (commands, res) => {
+async function processCommands (commands: Command[]) {
   for (const { command, directory, failOnText } of commands) {
     const { stdout } = await executeCommand(
       command,
@@ -90,19 +112,20 @@ const processCommands = async (commands, res) => {
     console.log(stdout);
 
     if (failOnText !== undefined && stdout.includes(failOnText)) {
-      reject(
-        new Error(
-          `Extra fail case occurred of '${failOnText}' for command '${command}'`
-        )
-      );
+      console.error(`Extra fail case occurred of '${failOnText}' for command '${command}'`);
+      throw new Error(`Extra fail case occurred of '${failOnText}' for command '${command}'`);
     }
   }
 };
 
 app.post("/", async (req, res, next) => {
   try {
-    const { commands } = config;
-    await processCommands(commands, res);
+    const { commands } = config as ConfigFile;
+    try {
+      await processCommands(commands);
+    } catch (error: any) {
+      res.status(500).json({message: "Got unexpected error!", error});
+    }
     res.status(200).json({ message: "Update commands completed successfully" });
 
     console.log(`Restarting pm2 instance ${serverName}`);
@@ -112,7 +135,7 @@ app.post("/", async (req, res, next) => {
     );
     //should be unreachable!
     console.error(stderr);
-  } catch (error) {
+  } catch (error: any) {
     console.log("Attempted marked as failing");
     res.status(500).json({ error: error.message });
   }
