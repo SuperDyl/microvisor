9c6e382b845d76c96ec120b5970789d5b49dbf14
Dylan <SuperDyl19@gmail.com>
2026-04-26 11:19:34 -0600

    Checked the file format at load time instead of request time.

--- update.ts
+++ update.ts
@@ -22,64 +22,81 @@ type Options = {
   dev: boolean;
 };
 
-const options: Options = yargs
-  .option("port", {
-    alias: "p",
-    describe: "The port the server listens on",
-    type: "number",
-    check: (port: number) => {
-      if (port < 1 || port > 65535) {
-        throw new Error("Port must be between 1 and 65535");
-      }
-      return true;
-    },
-  })
-  .option("serverName", {
-    alias: "l",
-    describe: "A name used in logs to represent this service",
-    type: "string",
-  })
-  .option("configPath", {
-    alias: "c",
-    describe: "path to the config file (assumed to be relative to the parent directory of updateHelper)",
-    type: "string",
-    default: "update-config.json",
-  })
-  .option("dev", {
-    describe: "Runs using a default development configPath",
-    type: "boolean",
-    default: false,
-  })
-  .demandOption(["port", "serverName"]).argv as Options;
+let _options: Options | null = null;
 
-if (options.dev) {
-  options.configPath = "dev-update-config.json";
+function getCommandLineOptions(): Options {
+  if (_options !== null) {
+    return _options;
+  }
+
+  const options: Options = yargs
+    .option("port", {
+      alias: "p",
+      describe: "The port the server listens on",
+      type: "number",
+      check: (port: number) => {
+        if (port < 1 || port > 65535) {
+          throw new Error("Port must be between 1 and 65535");
+        }
+        return true;
+      },
+    })
+    .option("serverName", {
+      alias: "l",
+      describe: "A name used in logs to represent this service",
+      type: "string",
+    })
+    .option("configPath", {
+      alias: "c",
+      describe: "path to the config file (assumed to be relative to the parent directory of updateHelper)",
+      type: "string",
+      default: "update-config.json",
+    })
+    .option("dev", {
+      describe: "Runs using a default development configPath",
+      type: "boolean",
+      default: false,
+    })
+    .demandOption(["port", "serverName"])
+    .argv as Options;
+
+  if (options.dev) {
+    options.configPath = "dev-update-config.json";
+  }
+
+  _options = options;
+  return _options
 }
-//path.join("../", options.configPath);
+
+function getConfigFromFile(configPath: string): ConfigFile {
+  let config: ConfigFile | null;
+  try {
+    const configData = fs.readFileSync(options.configPath, "utf8");
+    config = JSON.parse(configData);
+  } catch (error) {
+    throw new Error(`Error loading config file. Path=${options.configPath} error=${error}`);
+  }
+
+  if (!config) {
+    throw new Error(`Config file is not a valid json format! Check ${options.configPath}`);
+  }
+  if (config!.commands === undefined || !Array.isArray(config!.commands)) {
+    throw new Error(`Config commands section is incorrect Path=${options.configPath} commands=${config!.commands}`);
+  }
+
+  return config;
+}
+
+const options = getCommandLineOptions();
+const config = getConfigFromFile(options.configPath);
 
 const app = express();
 app.use(express.json());
 app.use(expressQueue({ activeLimit: 1, queuedLimit: 2 }));
 
-let config: ConfigFile | null = null;
-try {
-  const configData = fs.readFileSync(options.configPath, "utf8");
-  config = JSON.parse(configData);
-  // TODO: Confirm that config is the correct format
-} catch (error) {
-  console.error(`Error loading config file '${options.configPath}'`, error);
-  process.exit(1);
-}
-
-if (config === null) {
-  console.error(`File was in the wrong format '${options.configPath}'`);
-  process.exit(2);
-}
-
 // Helper function to execute a command in a directory
-async function executeCommand (command: string, directory: string): Promise<{stdout: string, stderr: string}> {
+async function executeCommand(command: string, directory: string): Promise<{ stdout: string, stderr: string }> {
   return new Promise((resolve, reject) => {
-    //const childProcess =
     exec(
       command,
       { cwd: directory },
@@ -97,18 +114,11 @@ async function executeCommand (command: string, directory: string): Promise<{std
         }
       }
     );
-
-    // childProcess.on("exit", (code: number) => {
-    //   if (code !== 0) {
-    //     console.error(`Command "${command}" exited with code ${code}`);
-    //     reject(new Error(`Command "${command}" exited with code ${code}`));
-    //   }
-    // });
   });
 };
 
 // Function to process the queue
-async function processCommands (commands: Command[]) {
+async function processCommands(commands: Command[]) {
   console.log(`Executing ${commands.length} commands.`)
   for (const { command, directory, failOnText } of commands) {
     const { stdout } = await executeCommand(
@@ -133,7 +143,7 @@ app.post("/", async (req, res, next) => {
     try {
       await processCommands(commands);
     } catch (error: any) {
-      res.status(500).json({message: "Got unexpected error!", error});
+      res.status(500).json({ message: "Got unexpected error!", error });
       return;
     }
     res.status(200).json({ message: "Update commands completed successfully" });
